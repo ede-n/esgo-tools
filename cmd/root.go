b2f904069bf6adce8b2239815b9eb1d36b36e3e4
Naveen Babu Ede <topcoderneo@gmail.com>
2021-03-27 04:38:52 -0700

    go run main.go

--- cmd/root.go
+++ cmd/root.go
@@ -6,8 +6,9 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"os"
+
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
@@ -18,13 +19,17 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "esgo-tools",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Tooling to interact with ElasticSearch written in GoLang!",
+	Long: `Bundles operations than span multiple REST api calls to ElasticSearch into intuitive commands.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Supported Operations:
+  * snapshot-repo
+    - create
+    - update
+    - delete
+  * snapshot
+    - create
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
